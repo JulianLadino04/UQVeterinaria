573bc09e525f495eec512f24a618f16ef7433e6e
ElJuanchito <perdomocardenas18@gmail.com>
2024-02-21 18:37:29 -0500

    Documente los metodos que tienen hasta ahora las clases.

--- workspace/labCollections/src/main/java/co/edu/uniquindio/labCollections/model/CarritoCompras.java
+++ workspace/labCollections/src/main/java/co/edu/uniquindio/labCollections/model/CarritoCompras.java
@@ -1,6 +1,45 @@
 package co.edu.uniquindio.labCollections.model;
 
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+/**
+ * Esta representacion sirve para gestionar los productos que un cliente desea comprar antes de realizar la compra, contiene un codigo y una lista de productos.
+ */
 public class CarritoCompras {
 
+    @EqualsAndHashCode.Include
     private String codigo;
+    private Set<String> lstProductos;
+
+    public CarritoCompras(String codigo) {
+        this.codigo = codigo;
+        this.lstProductos = new HashSet<>();
+    }
+
+    /**
+     * Agrega un producto a la lista (Al ser un Set no permite duplicados).
+     * @param codProducto que se desea agregar.
+     * @return un valor booleano que depende de si ya existe o no el codigo en la lista.
+     */
+    public boolean agregarProducto(String codProducto){
+        return lstProductos.add(codProducto);
+    }
+
+    /**
+     * Elimina un codigo de la lista.
+     * @param codProducto
+     * @return un valor booleano que depende de si existe o no el codigo en la lista.
+     */
+    public boolean elimarProducto(String codProducto){
+        return lstProductos.remove(codProducto);
+    }
+
 }
--- workspace/labCollections/src/main/java/co/edu/uniquindio/labCollections/model/Cliente.java
+++ workspace/labCollections/src/main/java/co/edu/uniquindio/labCollections/model/Cliente.java
@@ -6,14 +6,14 @@ import lombok.*;
 @Setter
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
+/**
+ * Modela un cliente para facilitar su identificacion en el sistema de gestion, contiene un nombre, direccion, numero de identificacion y un carrito de comprar para almacenar los productos previos a su compra.
+ */
 public class Cliente {
     private String nombre;
     private String direccion;
+    @EqualsAndHashCode.Include
     private String nIdentificacion;
     private CarritoCompras carritoCompras;
-    
-    
-
 }
