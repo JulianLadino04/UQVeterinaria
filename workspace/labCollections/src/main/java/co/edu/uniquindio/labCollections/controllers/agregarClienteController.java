8d1b28a04674b46344eb18fa337564111003661d
Breyner-sq <93687667+Breyner-sq@users.noreply.github.com>
2024-02-28 11:25:26 -0500

    agregarCliente fxml y controller

---
+++ workspace/labCollections/src/main/java/co/edu/uniquindio/labCollections/controllers/agregarClienteController.java
@@ -0,0 +1,55 @@
+package co.edu.uniquindio.labCollections.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class agregarClienteController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtIdentificacion;
+
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private Button btnAgregarCliente;
+
+    @FXML
+    void agregarClienteEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void direccionEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void identificacionEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nombreEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
