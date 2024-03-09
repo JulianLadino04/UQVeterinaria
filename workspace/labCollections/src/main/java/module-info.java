5bf6c99c9354577806cc8c336ff600061053b9d3
Breyner-sq <93687667+Breyner-sq@users.noreply.github.com>
2024-03-09 17:19:31 -0500

    new image
    new image names "products"

--- workspace/labCollections/src/main/java/module-info.java
+++ workspace/labCollections/src/main/java/module-info.java
@@ -1,9 +1,8 @@
-module co.edu.uniquindio {
+open module co.edu.uniquindio {
     requires javafx.controls;
     requires javafx.fxml;
     requires lombok;
 
-    opens co.edu.uniquindio.labCollections.controllers to javafx.fxml;
     exports co.edu.uniquindio;
 }
 
